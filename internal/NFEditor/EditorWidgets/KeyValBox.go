e200a0b4446e07024cd5c318dee21de10d0af658
Callial <callial@callial.com>
2024-04-13 14:55:00 -0600

    Local property saving working

--- internal/NFEditor/EditorWidgets/KeyValBox.go
+++ internal/NFEditor/EditorWidgets/KeyValBox.go
@@ -42,20 +42,31 @@ type KeyValBox struct {
 	keyEntry     *widget.Entry
 	valEntry     *TypedEntry
 	isEditing    bool
-	OnEditChange func(b *KeyValBox)
+	data         *map[string]interface{}
+	revertButton *widget.Button
+	gridBox      *fyne.Container
+	dataBox      *fyne.Container
+	buttonBox    *fyne.Container
 }
 
-func NewKeyValBox(key string, val interface{}) *KeyValBox {
+func NewKeyValBox(key string, data *map[string]interface{}) *KeyValBox {
+	val := (*data)[key]
 	box := &KeyValBox{
-		Container:  container.NewHBox(),
-		key:        key,
-		editButton: widget.NewButtonWithIcon("", theme.DocumentCreateIcon(), func() {}),
-		keyLabel:   widget.NewLabel(key),
-		valLabel:   NewTypedLabel(DetectValueType(val)),
-		keyEntry:   widget.NewEntry(),
-		valEntry:   NewTypedEntryWithText(DetectValueType(val)),
-		isEditing:  false,
+		Container:    container.NewPadded(),
+		gridBox:      container.NewGridWithColumns(2),
+		buttonBox:    container.NewHBox(),
+		dataBox:      container.NewGridWithColumns(2),
+		key:          key,
+		editButton:   widget.NewButtonWithIcon("", theme.DocumentCreateIcon(), func() {}),
+		revertButton: widget.NewButtonWithIcon("", theme.CancelIcon(), func() {}),
+		keyLabel:     widget.NewLabel(key),
+		valLabel:     NewTypedLabel(DetectValueType(val)),
+		keyEntry:     widget.NewEntry(),
+		valEntry:     NewTypedEntryWithText(DetectValueType(val)),
+		isEditing:    false,
+		data:         data,
 	}
+	box.Container.Add(box.gridBox)
 	box.editButton.OnTapped = func() {
 		if box.isEditing {
 			box.Save()
@@ -63,47 +74,81 @@ func NewKeyValBox(key string, val interface{}) *KeyValBox {
 			box.StartEditing()
 		}
 	}
+	box.revertButton.OnTapped = func() {
+		box.Revert()
+	}
 	box.keyEntry.SetText(key)
 	box.val = box.valEntry.Text
-	box.Container.Add(box.editButton)
-	box.Container.Add(box.keyLabel)
-	box.Container.Add(box.valLabel)
+	box.buttonBox.Add(box.editButton)
+	box.dataBox.Add(box.keyLabel)
+	box.dataBox.Add(box.valLabel)
+	box.gridBox.Add(box.dataBox)
+	box.gridBox.Add(box.buttonBox)
 	return box
 }
 
 func (b *KeyValBox) StartEditing() {
 	if !b.isEditing {
-		b.editButton.SetIcon(theme.DocumentSaveIcon())
-		b.Container.Remove(b.keyLabel)
-		b.Container.Remove(b.valLabel)
-		b.Container.Add(b.keyEntry)
-		b.Container.Add(b.valEntry)
 		b.isEditing = true
-		if b.OnEditChange != nil {
-			b.OnEditChange(b)
-		}
+		b.editButton.SetIcon(theme.DocumentSaveIcon())
+		b.dataBox.Objects = nil
+		b.gridBox.Objects = nil
+		b.buttonBox.Objects = nil
+		b.buttonBox.Add(b.revertButton)
+		b.buttonBox.Add(b.editButton)
+		b.dataBox.Add(b.keyEntry)
+		b.dataBox.Add(b.valEntry)
+		b.gridBox.Add(b.dataBox)
+		b.gridBox.Add(b.buttonBox)
 	}
 }
 
 func (b *KeyValBox) StopEditing(save bool) {
 	if b.isEditing {
-		if b.OnEditChange != nil {
-			b.OnEditChange(b)
-		}
-		b.Container.Remove(b.keyEntry)
-		b.Container.Remove(b.valEntry)
-		b.Container.Add(b.keyLabel)
-		b.Container.Add(b.valLabel)
+		b.isEditing = false
+		b.editButton.SetIcon(theme.DocumentCreateIcon())
+		b.buttonBox.Objects = nil
+		b.dataBox.Objects = nil
+		b.gridBox.Objects = nil
+		b.buttonBox.Add(b.editButton)
+		b.dataBox.Add(b.keyLabel)
+		b.dataBox.Add(b.valLabel)
+		b.gridBox.Add(b.dataBox)
+		b.gridBox.Add(b.buttonBox)
 		if save {
-			b.key = b.keyEntry.Text
-			b.val = b.valEntry.Text
-			b.keyLabel.SetText(b.key)
-			b.valLabel.SetText(b.val)
+			if b.key != b.keyEntry.Text {
+				//Remove the old key and add the new key
+				//Check if the new key already exists
+				if _, ok := (*b.data)[b.keyEntry.Text]; ok {
+					//dialog.ShowError(errors.New("key already exists"), l.window)
+					b.keyEntry.SetText(b.key)
+					return
+				} else {
+					(*b.data)[b.keyEntry.Text] = (*b.data)[b.key]
+					delete(*b.data, b.key)
+					b.key = b.keyEntry.Text
+					b.keyLabel.SetText(b.key)
+				}
+			}
+
+			//Check if the value has changed
+			if b.val != b.valEntry.Text {
+				val, err := b.valEntry.ParsedValue()
+				if err != nil {
+					//dialog.ShowError(err, l.window)
+					b.valEntry.SetText(b.val)
+					return
+				}
+				(*b.data)[b.key] = val
+				b.val = b.valEntry.Text
+				b.valLabel.SetText(b.val)
+			}
 		} else {
 			b.keyEntry.SetText(b.key)
+			b.keyLabel.SetText(b.key)
 			b.valEntry.SetText(b.val)
+			b.valLabel.SetText(b.val)
 		}
-		b.isEditing = false
 	}
 }
 
--- internal/NFEditor/EditorWidgets/TypedParamList.go
+++ internal/NFEditor/EditorWidgets/TypedParamList.go
@@ -1,10 +1,8 @@
 package EditorWidgets
 
 import (
-	"errors"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	"sort"
 )
@@ -33,10 +31,9 @@ func (t TypedParamListRenderer) Refresh() {
 
 type TypedParamList struct {
 	widget.BaseWidget
-	container  *fyne.Container
-	data       *map[string]interface{}
-	editingKey string
-	window     fyne.Window
+	container *fyne.Container
+	data      *map[string]interface{}
+	window    fyne.Window
 }
 
 func (l *TypedParamList) CreateRenderer() fyne.WidgetRenderer {
@@ -66,59 +63,8 @@ func (l *TypedParamList) SetData(data *map[string]interface{}) {
 
 	// Iterate over sorted keys
 	for _, key := range keys {
-		value := (*data)[key]
-		box := NewKeyValBox(key, value)
-		box.OnEditChange = func(b *KeyValBox) {
-			if l.editingKey != b.key {
-				l.ChangeEditingKey(l.editingKey, b.key)
-			}
-		}
+		box := NewKeyValBox(key, data)
 		l.container.Add(box)
 	}
 	l.Refresh()
-}
-
-func (l *TypedParamList) ChangeEditingKey(oldKey string, newKey string) {
-	//Pop up a dialog asking if the user wants to save the changes made to the old key before switching
-	if oldKey != "" {
-		dialog.ShowConfirm("Save Changes?", "Do you want to save the changes made to "+oldKey+"?", func(b bool) {
-			if b {
-				l.SaveChanges(oldKey)
-			}
-			l.editingKey = newKey
-		}, l.window)
-	} else {
-		l.editingKey = newKey
-	}
-}
-
-func (l *TypedParamList) SaveChanges(key string) {
-	for _, obj := range l.container.Objects {
-		box := obj.(*KeyValBox)
-		if box.key == key {
-			//Check if the key is the same as the entryKey.Text
-			if box.key != box.keyEntry.Text {
-				//Check if the new key already exists
-				if _, ok := (*l.data)[box.keyEntry.Text]; ok {
-					dialog.ShowError(errors.New("key already exists"), l.window)
-					return
-				} else {
-					//Delete the old key and add the new key
-					(*l.data)[box.keyEntry.Text] = (*l.data)[box.key]
-					delete(*l.data, box.key)
-					box.key = box.keyEntry.Text
-				}
-			} else {
-				//Check if the value has changed
-				if box.val != box.valEntry.Text {
-					val, err := box.valEntry.ParsedValue()
-					if err != nil {
-						dialog.ShowError(err, l.window)
-						return
-					}
-					(*l.data)[box.key] = val
-				}
-			}
-		}
-	}
 }
